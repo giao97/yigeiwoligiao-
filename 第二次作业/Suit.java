22818a6cb8e347ad093b0dc967d67b0b5b0c0cb3
giao97 <649076133@qq.com>
2020-03-13 17:09:00 +0800

    second work

---
+++ 第二次作业/Suit.java
@@ -0,0 +1,15 @@
+package text;
+
+public enum Suit{
+    SPADE(1),HEART(2),CLUB(3),DIAMOND(4);
+    private int index;
+
+    public int getIndex() {
+        return index;
+    }
+
+    Suit(int index){
+        this.index=index;
+    }
+}
+
